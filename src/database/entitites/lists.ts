ad2bf59cf52c5864ce4f4e512a90ca8e8cd05daa
Marcos Vinicius <marcos.v.silva@live.com>
2023-05-22 00:06:44 +0100

    Refactoring code with new style

--- src/database/entitites/lists.ts
+++ src/database/entitites/lists.ts
@@ -1,56 +1,56 @@
-import { DatabaseTable, Entity, SchemaMapping } from "./database";
+import { DatabaseTable, Entity, SchemaMapping } from './database';
 
 export enum StatusList {
-    Create = 0,
-    InProgress = 1,
-    Complete = 2,
-    Canceled = 3
+  Create = 0,
+  InProgress = 1,
+  Complete = 2,
+  Canceled = 3,
 }
 
 export interface List extends DatabaseTable {
-    name: String;
-    status: StatusList;
-    total_list: Number;
-    discount: Number;
-    date_completed: Date;
+  name: String;
+  status: StatusList;
+  total_list: Number;
+  discount: Number;
+  date_completed: Date;
 
-    //database
-    id: number;
-    uuid: string;
-    createdAt: Date;
-    updatedAt: Date;
-};
+  //database
+  id: number;
+  uuid: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 const listColumns: SchemaMapping<List> = {
-    id: 'id',
-    uuid: 'uuid',
-    name: 'name',
-    status: 'status',
-    total_list: 'total_list',
-    discount: 'discount',
-    date_completed: 'date_completed',
-    createdAt: 'createdAt',
-    updatedAt: 'updatedAt'
-}
+  id: 'id',
+  uuid: 'uuid',
+  name: 'name',
+  status: 'status',
+  total_list: 'total_list',
+  discount: 'discount',
+  date_completed: 'date_completed',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+};
 
 const listMapping: SchemaMapping<List> = {
-    id: 'id',
-    uuid: 'uuid',
-    name: 'name',
-    status: 'status',
-    total_list: 'total_list',
-    discount: 'discount',
-    date_completed: 'date_completed',
-    createdAt: 'createdAt',
-    updatedAt: 'updatedAt'
-}
+  id: 'id',
+  uuid: 'uuid',
+  name: 'name',
+  status: 'status',
+  total_list: 'total_list',
+  discount: 'discount',
+  date_completed: 'date_completed',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+};
 
 const Lists: Entity<List> = {
-    name: 'List',
-    table_name: 'Lists',
-    column: listColumns,
-    mapping: listMapping,
-    allowed: [listColumns.name, listColumns.status]
-}
+  name: 'List',
+  table_name: 'Lists',
+  column: listColumns,
+  mapping: listMapping,
+  allowed: [listColumns.name, listColumns.status],
+};
 
 export default Lists;
