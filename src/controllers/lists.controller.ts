1b08a27e46dfc2f050a11734ac5c45926bd26d62
Marcos Vinicius <marcos.v.silva@live.com>
2023-06-16 17:44:03 +0100

    [refactor] listing lists with products

--- src/controllers/lists.controller.ts
+++ src/controllers/lists.controller.ts
@@ -1,39 +1,42 @@
 import { Request, Response } from 'express';
 import { JsonObject } from 'type-fest';
 
-import knex from '../database';
-import ListProducts from '../database/entitites/list_products';
-import Lists, { StatusList } from '../database/entitites/lists';
+import { DatabaseTable } from '../database/entitites/database';
+import ListProducts, {
+  selectColumnsListProducts,
+  StatusListProduct,
+} from '../database/entitites/list_products';
+import Lists, { selectColumnsLists } from '../database/entitites/lists';
 import Products from '../database/entitites/products';
 import { MissingParamsException, MissingReferencesFieldsException } from '../exceptions';
 import { formatReferenceFieldUUId, isEmpty } from '../utils';
-import { baseIndex, baseRemove, baseShow, baseUpdate } from './application.controller';
-import { create as databaseCreate } from './database.controller';
-
-const selectColumns = [
-  knex.ref(Lists.mapping.uuid).as(Lists.column.uuid),
-  knex.ref(Lists.mapping.name).as(Lists.column.name),
-  knex.ref(Lists.mapping.status).as(Lists.column.status),
-  knex.ref(Lists.mapping.total_list).as(Lists.column.total_list),
-  knex.ref(Lists.mapping.discount).as(Lists.column.discount),
-  knex.ref(Lists.mapping.date_completed).as(Lists.column.date_completed),
-  knex.ref(Lists.mapping.createdAt).as(Lists.column.createdAt),
-  knex.ref(Lists.mapping.updatedAt).as(Lists.column.updatedAt),
-];
-
-const selectColumnsListProducts = [
-  knex.ref(ListProducts.mapping.uuid).as(ListProducts.column.uuid),
-  knex.ref(ListProducts.mapping.date).as(ListProducts.column.date),
-  knex.ref(ListProducts.mapping.status).as(ListProducts.column.status),
-  knex.ref(ListProducts.mapping.createdAt).as(ListProducts.column.createdAt),
-  knex.ref(ListProducts.mapping.updatedAt).as(ListProducts.column.updatedAt),
-];
+import { baseRemove, baseShow, baseUpdate } from './application.controller';
+import { create as databaseCreate, getAll as databaseIndex } from './database.controller';
+import { getAllByList } from './products.controller';
 
 /**
  * @route GET /api/lists
  */
 export async function index(req: Request, res: Response): Promise<Response> {
-  return baseIndex(res, Lists, selectColumns);
+  try {
+    const lists = await databaseIndex(selectColumnsLists, Lists);
+
+    if (lists && Array(lists).length > 0) {
+      for (const index in lists) {
+        const products = await getAllByList((lists[index] as DatabaseTable).uuid);
+
+        if (products && (products as DatabaseTable[]).length > 0) {
+          lists[index]['products'] = products;
+        }
+      }
+
+      return res.status(200).json(lists).end();
+    }
+    return res.status(200).end();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).end();
+  }
 }
 
 /**
@@ -41,7 +44,7 @@ export async function index(req: Request, res: Response): Promise<Response> {
  * @param {string} uuid
  */
 export async function show(req: Request, res: Response): Promise<Response> {
-  return baseShow(res, req.params.uuid, Lists, selectColumns);
+  return baseShow(res, req.params.uuid, Lists, selectColumnsLists);
 }
 
 /**
@@ -50,31 +53,17 @@ export async function show(req: Request, res: Response): Promise<Response> {
 export async function create(req: Request, res: Response): Promise<Response | void> {
   if (req.body) {
     try {
-      const list = await databaseCreate(req.body, Lists, selectColumns);
+      const list = await databaseCreate(req.body, Lists, selectColumnsLists);
 
-      if (list && !isEmpty(list['uuid'] as string)) {
+      if (list) {
         if (req.body['products']) {
-          const products = Array.from(req.body['products']);
-
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          products.forEach(async (prod) => {
-            const form: JsonObject = {
-              [formatReferenceFieldUUId(Lists)]: list['uuid'] as string,
-              [formatReferenceFieldUUId(Products)]: prod as string,
-              date: new Date().toJSON(),
-              status: StatusList.Create,
-            };
-
-            const listProduct = await databaseCreate(form, ListProducts, selectColumnsListProducts);
-
-            if (!listProduct || isEmpty(listProduct['uuid'] as string)) {
-              console.error(`error to link product: ${prod} in ${list['uuid'] as string} list`);
-            }
-          });
+          linkProducts(list['uuid'], req.body['products'] as string[]);
         }
         return res.status(201).json(list).end();
       }
+      return res.status(500).end();
     } catch (error) {
+      console.error(error);
       if (
         error instanceof MissingParamsException ||
         error instanceof MissingReferencesFieldsException
@@ -92,7 +81,7 @@ export async function create(req: Request, res: Response): Promise<Response | vo
  * @param {string} uuid
  */
 export async function update(req: Request, res: Response): Promise<Response> {
-  return baseUpdate(res, req.params.uuid, req.body, Lists, selectColumns);
+  return baseUpdate(res, req.params.uuid, req.body, Lists, selectColumnsLists);
 }
 
 /**
@@ -101,4 +90,24 @@ export async function update(req: Request, res: Response): Promise<Response> {
  */
 export async function remove(req: Request, res: Response): Promise<Response | void> {
   return baseRemove(res, req.params.uuid, req.body, Lists);
+}
+
+function linkProducts(listUUID: string, products: string[]): void {
+  if (Array.isArray(products)) {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    products.forEach(async (prod) => {
+      const form: JsonObject = {
+        [formatReferenceFieldUUId(Lists)]: listUUID,
+        [formatReferenceFieldUUId(Products)]: prod,
+        date: new Date().toJSON(),
+        status: JSON.stringify(StatusListProduct.Create),
+      };
+
+      const listProduct = await databaseCreate(form, ListProducts, selectColumnsListProducts);
+
+      if (!listProduct || isEmpty(listProduct['uuid'] as string)) {
+        console.error(`error to link product: ${prod} in ${listUUID} list`);
+      }
+    });
+  }
 }
