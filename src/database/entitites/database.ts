37f7d18372ac5dfeedbd0194cbaa3160f426a0a0
Marcos Vinicius <marcos.v.silva@live.com>
2023-06-23 12:41:06 +0100

    [chore] update model entity to add colums select property

--- src/database/entitites/database.ts
+++ src/database/entitites/database.ts
@@ -1,19 +1,31 @@
+import { Knex } from 'knex';
+
 export type SchemaMapping<T> = {
   [Name in keyof T]: string;
 };
+
+export type Ref = Knex.Ref<
+  string,
+  {
+    [x: string]: string;
+  }
+>[];
 
 export type Entity<T> = {
   // Model name
   name: string;
 
   // Table name
-  table_name: string;
+  tableName: string;
 
   // Application-level column names
   column: SchemaMapping<T>;
 
   // Database-level column names
   mapping: SchemaMapping<T>;
+
+  // Select columns
+  selectColumsRef: Ref;
 
   // References
   reference?: Array<Entity<DatabaseTable>>;
