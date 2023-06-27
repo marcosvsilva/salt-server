6434946cd060ddde41e3f8b89e7d29713396c45b
Marcos Vinicius <marcos.v.silva@live.com>
2023-06-27 17:59:35 +0100

    [feat] return owner (users) of list on  list index

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 
+import knex from '../database';
+import Lists from '../database/entitites/lists';
 import Users from '../database/entitites/users';
+import { User } from '../database/models';
+import { InvalidUUIDException } from '../exceptions';
+import { deserialize, formatReferenceFieldUUId, isValidUUID } from '../utils';
 import { baseCreate, baseIndex, baseRemove, baseShow, baseUpdate } from './application.controller';
 
 /**
@@ -39,4 +44,32 @@ export async function update(req: Request, res: Response): Promise<Response> {
  */
 export async function remove(req: Request, res: Response): Promise<Response> {
   return baseRemove(res, req.params.uuid, Users);
+}
+
+export async function getByList(list_uuid: string): Promise<User> {
+  if (!isValidUUID(list_uuid)) {
+    throw new InvalidUUIDException();
+  }
+
+  return knex
+    .select(Users.selectColumsRef)
+    .from(Users.tableName)
+    .leftJoin(
+      Lists.tableName,
+      `${Lists.tableName}.${formatReferenceFieldUUId(Users)}`,
+      `${Users.tableName}.${Users.mapping.uuid}`,
+    )
+    .where(`${Lists.tableName}.${Lists.mapping.uuid}`, list_uuid)
+    .limit(1)
+    .first()
+    .then((entries) => {
+      const file = deserialize(entries, Users);
+      if (Array.isArray(file)) {
+        return file[0] as User;
+      }
+      return file as User;
+    })
+    .catch((error: Error) => {
+      throw error;
+    });
 }
