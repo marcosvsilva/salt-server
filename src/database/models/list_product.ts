f9ea1111ddc5c45266ee5af35f2e891052655d39
Marcos Vinicius <marcos.v.silva@live.com>
2023-07-04 23:57:47 +0100

    [refactor] refactor controllers with new validations and exceptions classes and functions

---
+++ src/database/models/list_product.ts
@@ -0,0 +1,20 @@
+import { DatabaseTable } from '../entitites/database';
+
+export enum StatusListProduct {
+  Create = 0,
+  InQueue = 1,
+  Bought = 2,
+}
+
+export interface ListProduct extends DatabaseTable {
+  date: Date;
+  status: StatusListProduct;
+
+  // database
+  id: number;
+  uuid: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default ListProduct;
