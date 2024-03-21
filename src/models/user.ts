dc17e475b65b906229a8e04ee6289ebf67230804
Marcos Vinicius <marcos.v.silva@live.com>
2024-03-21 23:45:28 +0000

    [feat]: creating a new model layer

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,18 @@
 import { DatabaseTable } from './database';
 
+export enum StatusUser {
+  Active = 0,
+  Inactive = 1,
+  Removed = 2,
+}
+
+export enum TypeUser {
+  User = 0,
+  Staff = 1,
+  Supervisor = 2,
+  Manager = 3,
+}
+
 export interface User extends DatabaseTable {
   first_name: string;
   last_name: string;
@@ -8,6 +21,8 @@ export interface User extends DatabaseTable {
   birthday: Date;
   email: string;
   contact: string;
+  status: StatusUser;
+  type: TypeUser;
 
   // database
   id: number;
