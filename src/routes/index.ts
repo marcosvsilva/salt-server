233a35727ee926e8742b222eb7ed100bd85fda79
Marcos Vinicius <marcos.v.silva@live.com>
2024-03-26 23:34:01 +0000

    feat: starting adapt a new routes controllers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,6 @@
 import express, { Request, Response, Router } from 'express';
 
-import * as listsController from '../controllers/lists.controller';
-import * as pricesController from '../controllers/prices.controller';
-import * as productsController from '../controllers/products.controller';
-import * as usersController from '../controllers/users.controller';
+import { PriceController } from '../controllers/prices.controller';
 
 const router: Router = express.Router();
 
@@ -11,45 +8,57 @@ const router: Router = express.Router();
  * Lists
  */
 
-const routerList = '/api/lists';
-router.get(routerList, listsController.index);
-router.get(`${routerList}/:uuid`, listsController.show);
-router.post(routerList, listsController.create);
-router.put(`${routerList}/:uuid`, listsController.update);
-router.delete(`${routerList}/:uuid`, listsController.remove);
+// const routerList = '/api/lists';
+// router.get(routerList, listsController.index);
+// router.get(`${routerList}/:uuid`, listsController.show);
+// router.post(routerList, listsController.create);
+// router.put(`${routerList}/:uuid`, listsController.update);
+// router.delete(`${routerList}/:uuid`, listsController.remove);
 
 /**
  * Users
  */
 
-const routerUser = '/api/users';
-router.get(routerUser, usersController.index);
-router.get(`${routerUser}/:uuid`, usersController.show);
-router.post(routerUser, usersController.create);
-router.put(`${routerUser}/:uuid`, usersController.update);
-router.delete(`${routerUser}/:uuid`, usersController.remove);
+// const routerUser = '/api/users';
+// router.get(routerUser, usersController.index);
+// router.get(`${routerUser}/:uuid`, usersController.show);
+// router.post(routerUser, usersController.create);
+// router.put(`${routerUser}/:uuid`, usersController.update);
+// router.delete(`${routerUser}/:uuid`, usersController.remove);
 
 /**
  * Products
  */
 
-const routerProducts = '/api/products';
-router.get(routerProducts, productsController.index);
-router.get(`${routerProducts}/:uuid`, productsController.show);
-router.post(routerProducts, productsController.create);
-router.put(`${routerProducts}/:uuid`, productsController.update);
-router.delete(`${routerProducts}/:uuid`, productsController.remove);
+// const routerProducts = '/api/products';
+// router.get(routerProducts, productsController.index);
+// router.get(`${routerProducts}/:uuid`, productsController.show);
+// router.post(routerProducts, productsController.create);
+// router.put(`${routerProducts}/:uuid`, productsController.update);
+// router.delete(`${routerProducts}/:uuid`, productsController.remove);
 
 /**
  * Prices
  */
 
+// Create an instance of PriceController
+const priceController = new PriceController();
 const routerPrices = '/api/prices';
-router.get(routerPrices, pricesController.index);
-router.get(`${routerPrices}/:uuid`, pricesController.show);
-router.post(routerPrices, pricesController.create);
-router.put(`${routerPrices}/:uuid`, pricesController.update);
-router.delete(`${routerPrices}/:uuid`, pricesController.remove);
+
+// GET /api/prices
+router.get(routerPrices, priceController.index);
+
+// GET /api/prices/:uuid
+router.get(`${routerPrices}/:uuid`, priceController.show);
+
+// POST /api/prices
+router.post(routerPrices, priceController.create);
+
+// PUT /api/prices/:uuid
+router.put(`${routerPrices}/:uuid`, priceController.update);
+
+// DELETE /api/prices/:uuid
+router.delete(`${routerPrices}/:uuid`, priceController.delete);
 
 /**
  * Root
