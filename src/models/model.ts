f738798424cf4d32a773e4dd771d9020e3eeca46
Marcos Vinicius <marcos.v.silva@live.com>
2024-04-05 23:43:53 +0100

    [feat]: improving new model classes and database scheme

---
+++ src/models/model.ts
@@ -0,0 +1,11 @@
+export interface InterfaceModel {
+  // identity
+  id: number;
+  uuid: string;
+
+  // tracking control
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default InterfaceModel;
