801c3b5aa77d6525a7c6c2751b62ca574632ca7d
Marcos Vinicius <marcos.v.silva@live.com>
2024-04-07 21:00:51 +0100

    [feat]: updating a new database scheme

---
+++ src/database/entitites/entity.ts
@@ -0,0 +1,46 @@
+import { Knex } from 'knex';
+
+export type SchemaMapping<T> = {
+  [Name in keyof T]: string;
+};
+
+export type Ref = Knex.Ref<
+  string,
+  {
+    [x: string]: string;
+  }
+>[];
+
+export type ForeignKey = {
+  uuid: string;
+  references: string;
+  table: string;
+};
+
+export type Entity<T> = {
+  // Model name
+  name: string;
+
+  // Table name
+  tableName: string;
+
+  // Application-level attributes names
+  column: SchemaMapping<T>;
+
+  // Database-level attributes names
+  mapping: SchemaMapping<T>;
+
+  // Foreign keys
+  foreignKeys?: ForeignKey[];
+
+  // Select attributes
+  selectColumsRef: Ref;
+
+  // JSON attributes
+  json?: string[];
+
+  // Allowed columns in API transactions
+  allowed: Array<string | undefined>;
+};
+
+export default Entity;
