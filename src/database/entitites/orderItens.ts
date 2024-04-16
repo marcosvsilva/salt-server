5285ec5dda3ec8358ab106980b0a5d5426d5e42b
Marcos Vinicius <marcos.v.silva@live.com>
2024-04-16 23:51:56 +0100

    [refactor] updating the persistence layer uses and imports

---
+++ src/database/entitites/orderItens.ts
@@ -0,0 +1,55 @@
+import { InterfaceModel } from '../../models';
+import knex from '..';
+import { Entity, ForeignKey, SchemaMapping } from './entity';
+import { Itens } from './itens';
+import { Orders } from './orders';
+
+const orderItensColumns: SchemaMapping<InterfaceModel> = {
+  id: 'id',
+  uuid: 'uuid',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+};
+
+const orderItensMapping: SchemaMapping<InterfaceModel> = {
+  id: 'id',
+  uuid: 'uuid',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+};
+
+const schemaName = 'Salt';
+const tabName = 'Categories';
+
+const selectColumnsListProducts = [
+  knex.ref(orderItensColumns.id).as(orderItensMapping.id).withSchema(tabName),
+  knex.ref(orderItensColumns.uuid).as(orderItensMapping.uuid).withSchema(tabName),
+  knex.ref(orderItensColumns.createdAt).as(orderItensMapping.createdAt).withSchema(tabName),
+  knex.ref(orderItensColumns.updatedAt).as(orderItensMapping.updatedAt).withSchema(tabName),
+];
+
+export const OrderItens: Entity<InterfaceModel> = {
+  name: schemaName,
+  tableName: tabName,
+  column: orderItensColumns,
+  mapping: orderItensMapping,
+  selectColumsRef: selectColumnsListProducts,
+  allowed: [],
+};
+
+const categoryForeignKeys: ForeignKey[] = [
+  {
+    attribute: 'order',
+    references: Orders.column.uuid,
+    table: Orders.tableName,
+  },
+  {
+    attribute: 'itens',
+    references: Itens.column.uuid,
+    table: Itens.tableName,
+  },
+];
+
+OrderItens.foreignKeys = categoryForeignKeys;
+
+export default OrderItens;
