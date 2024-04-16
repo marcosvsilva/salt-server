a072fe00974ec21de6cba0e8f345efaccfe605fe
Marcos Vinicius <marcos.v.silva@live.com>
2024-04-16 23:52:23 +0100

    [refactor] updating the persistence layer uses and imports

---
+++ src/database/entitites/index.ts
@@ -0,0 +1,9 @@
+export { Brands } from './brands';
+export { Categories } from './categories';
+export { Entity, ForeignKey } from './entity';
+export { Itens } from './itens';
+export { OrderItens } from './orderItens';
+export { Orders } from './orders';
+export { Payments } from './payments';
+export { Prices } from './prices';
+export { Users } from './users';
