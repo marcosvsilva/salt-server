42f6fa7002d1d4c31827a411df95ceec2f6d70a0
Marcos Vinicius <marcos.v.silva@live.com>
2024-04-17 23:48:51 +0100

    [refactor]: fixing issues with migrations inconsistences

--- src/database/migrations/20240405233802_orderItens.ts
+++ src/database/migrations/20240405233802_orderItens.ts
@@ -1,7 +1,6 @@
 import { Knex } from 'knex';
 
-import { ForeignKey } from '../entitites/entity';
-import OrderItens from '../entitites/orderItem';
+import { ForeignKey, OrderItens } from '../entitites';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema
@@ -19,7 +18,10 @@ export async function up(knex: Knex): Promise<void> {
         // References
         if (OrderItens.foreignKeys) {
           OrderItens.foreignKeys.forEach((foreignKey: ForeignKey) => {
-            table.uuid(foreignKey.uuid).references(foreignKey.references).inTable(foreignKey.table);
+            table
+              .uuid(foreignKey.attribute)
+              .references(foreignKey.references)
+              .inTable(foreignKey.table);
           });
         }
 
