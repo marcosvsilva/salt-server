ecfb92a65f1f4ecd0328f1b4cb90fe6af8875720
Marcos Vinicius <marcos.v.silva@live.com>
2024-04-24 22:52:25 +0100

    [feat]: creating new controllers for the application server

---
+++ src/controllers/index.ts
@@ -0,0 +1,7 @@
+export { BrandController } from './brand.controller';
+export { CategoryController } from './category.controller';
+export { ItemController } from './item.controller';
+export { OrderController } from './order.controller';
+export { PaymentController } from './payment.controller';
+export { PriceController } from './price.controller';
+export { UserController } from './user.controller';
